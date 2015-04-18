db2af0df466e7914b1f417693205b9499b6df623
liquid0624 <rjbirnesser@gmail.com>
2015-04-18 16:14:57 -0400

    shamu: fix derps and include overlay now
    Thanks dhacker :)

--- deso.mk
+++ deso.mk
@@ -1,4 +1,4 @@
-# Device resolution
+# Boot animation
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 2560
 
@@ -13,6 +13,8 @@ PRODUCT_SCREEN_RES := $(TARGET_SCREEN_WIDTH) x $(TARGET_SCREEN_HEIGHT)
 
 #Inherit some common Desolation stuff.
 $(call inherit-product, vendor/deso/config/common.mk)
+
+# Enhanced NFC
 $(call inherit-product, vendor/deso/config/telephony.mk)
 
 # Enhanced NFC
@@ -20,6 +22,8 @@ $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := deso_shamu
